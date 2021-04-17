bdd855578424a8c1f6c2c24f930857edbfd85270
Pratik Jagade <pratik.21810767@viit.ac.in>
2021-04-17 17:54:30 +0530

    dotenv configured

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const routes = require('./api/doc/doc.routes');
 const app=express();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+console.log('user : ', process.env.DB_USER);
 
 app.get('/',routes);
 
